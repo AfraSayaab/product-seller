eff858ee40b523bf626f28b1a7ae311760d1903a
Zoya Arshad <zoya09854@gmail.com>
2026-01-16 18:05:30 +0500

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
       </div>
 
       {/* MAIN FOOTER */}
-      <div className="max-w-7xl mx-auto px-6 py-20">
+      <div className="max-w-7xl mx-auto px-6 pt-20 pb-5">
 
         {/* 4 COLUMNS WITH PINK LINES */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 relative">
